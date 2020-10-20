5f2a6e211c92b257f4398f96e4f0f6921d5aedad
Avi-000-Avi <atoppo267@gmail.com>
2020-10-21 00:00:31 +0530

    playback

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
+const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myVideo = document.createElement('video');
-myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+    path: '/peerjs',
+    host: '/',
+    port: '3030'
+})
 
 let myVideoStream;
+myVideo.muted = true;
 
 //output audio and video from the browser
 navigator.mediaDevices.getUserMedia({
@@ -10,9 +17,33 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     myVideoStream = stream;
-    addVideoStream(myVideo,stream);
-    
+    addVideoStream(myVideo,stream); 
+
+    peer.on('call',call => {
+    call.answer(stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+      })
+    })
+      
+    socket.on('user-connected', (userId) => {
+        connecToNewUser(userId,stream);
+    })
 })
+
+peer.on('open',id => {
+    socket.emit('join-room', ROOM_ID, id);
+});
+
+
+const connecToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+}
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
